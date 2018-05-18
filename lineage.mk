68170dc87089c7c4f924ad5e590412b79e1a73a0
Montana <39386889+moto8937montana@users.noreply.github.com>
2018-05-18 17:44:49 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -26,7 +26,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := montanamontana
+PRODUCT_DEVICE := montana
 PRODUCT_NAME := lineage_montana
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
