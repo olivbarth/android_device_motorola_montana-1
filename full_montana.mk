6ffc5ee758c2557b71ccb83b1d75c4fab34b1c37
OlivierB <33780394+olivbarth@users.noreply.github.com>
2018-05-20 09:31:01 +0200

    Update full_montana.mk

--- full_montana.mk
+++ full_montana.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from montana device
-$(call inherit-product, device/motorola/montana/montana.mk)
+$(call inherit-product, device/motorola/montana/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := montana
